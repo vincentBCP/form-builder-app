b32ccd212485957206bb02faadad3e905e3a5651
Vincent <vincentbenjaminpatoc@gmail.com>
2025-04-05 12:17:01 +0800

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,7 @@ import FormBuilder from "@vincentbcp/form-builder";
 const App = () => {
   return (
     <div>
-      <FormBuilder
-        onSave={(formDef) => {
-          const name = prompt("Please enter form name?");
-
-          if (!name) return;
-
-          const dataSrc =
-            "data:text/json;charset=utf-8," +
-            encodeURIComponent(JSON.stringify(formDef));
-          var a = document.createElement("a");
-          document.body.appendChild(a);
-          a.style.display = "none";
-          a.href = dataSrc;
-          a.download = `${name}.json`;
-          a.click();
-          document.body.removeChild(a);
-        }}
-      />
+      <FormBuilder onSave={() => {}} />
     </div>
   );
 };
